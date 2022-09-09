88e1ce1897e5e9813956350ef56c03e84541f199
zheyuan <835047078@qq.com>
2022-09-09 11:13:23 +0800

    modify xargs.c with all tests passed

--- lab1-Xv6 and Unix utilities/xargs.c
+++ lab1-Xv6 and Unix utilities/xargs.c
@@ -8,68 +8,53 @@ void
 execCommand(char* Command, char* Argv[MAXARG])
 {
     if(fork() == 0)
-    {
-        // printf("5\n");
         exec(Command, Argv);
-        // printf("6\n");
-    }
     else
-    {
-        // printf("7\n");
         wait(0);
-    }
+    
 }
 
 int 
 main(int argc, char* argv[])
 {
-    char CmdPath[512] = "./user/", *Ptr;
+    char CmdPath[512] = "", *Ptr;
     char* Argv[MAXARG];                                     // the array is used to convey param to command
-    int StartIndex = argc - 2;                              // how many arguments original command line has kept
+    int StartIndex = argc - 1;                              // how many arguments original command line has kept
 
     /* E.g: xargs grep hello */
     /* parse the original command */
     Ptr = CmdPath + strlen(CmdPath);
     memmove(Ptr, argv[1],  strlen(argv[1]));
     Ptr[strlen(argv[1])] = 0;                               // get the executable file's path
-    printf("the command is %s\n", CmdPath);
 
-    // printf("argc is %d\n", argc);
-    for(int i = 0 ; i < argc ; ++i)
-        printf("argv[%d] is %s\n", i, argv[i]);
-
-    for(int i = 2 ; i < argc ; ++i)                         // copy the orginal param to Argv
+    for(int i = 1 ; i < argc ; ++i)                         // copy the orginal param to Argv
     {
-        Ptr = Argv[i - 2] + strlen(Argv[i - 2]);
-
-        memmove(Ptr, argv[i], strlen(argv[i])); 
-        Ptr[strlen(argv[i])] = 0;
+        uint Length = strlen(argv[i]) + 1;
+        Argv[i - 1] = malloc(Length); 
+        memmove(Argv[i - 1], argv[i], Length); 
     }
-    
-    printf("Argv[0] is %s\n", Argv[0]);
-
-    // printf("2\n");
+    Argv[StartIndex + 1] = 0;                               // set the next element of Argv as NULL
 
     char ReadChar;
-    Ptr = Argv[StartIndex];                                 // move Ptr to the begining of new param 
+    Argv[StartIndex] = malloc(512);                         // allocate a 512 bytes space for additional params
+    Ptr = Argv[StartIndex];                                 // move Ptr to the begining of new param                         
     while(read(0, &ReadChar, 1))                            // if not reach the EOF
     {
         if(ReadChar != '\n')
         {
-            // printf("3\n");
             *Ptr = ReadChar;
             ++Ptr;
         }
         else
         {
-            // printf("4\n");
             *Ptr = 0;                                       // set the terminal flag
-            for(int i = 0 ; i <= StartIndex ; ++i)
-                printf("the current param passed by xargs is %s\n", Argv[i]);
             execCommand(CmdPath, Argv);     
             Ptr = Argv[StartIndex];                         // reset Ptr to the begining of new param
         }
     }
+
+    for(int i = 0 ; i <= StartIndex ; ++i)                  // release the memory
+        free(Argv[i]);
 
     exit(0);
 }
